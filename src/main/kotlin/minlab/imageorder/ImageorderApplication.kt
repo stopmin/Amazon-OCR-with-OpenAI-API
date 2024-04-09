8e9f200d57a33966e6309177bea40409e186092d
stopmin <geemin0@naver.com>
2024-04-09 16:46:36 +0900

    feat: project initialize

---
+++ src/main/kotlin/minlab/imageorder/ImageorderApplication.kt
@@ -0,0 +1,11 @@
+package minlab.imageorder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImageorderApplication
+
+fun main(args: Array<String>) {
+	runApplication<ImageorderApplication>(*args)
+}
