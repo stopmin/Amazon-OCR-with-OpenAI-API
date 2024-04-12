cc5ba34ed9c41159829b028460e8d567f303199d
stopmin <geemin0@naver.com>
2024-04-12 17:24:21 +0900

    build: kotlin coroutine & webflux 의존성 추가 - 비동기 controller 이슈.. - 추후 삭제할지도!

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,47 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.4"
-	id("io.spring.dependency-management") version "1.1.4"
-	kotlin("jvm") version "1.9.23"
-	kotlin("plugin.spring") version "1.9.23"
+    id("org.springframework.boot") version "3.2.4"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
 }
 
 group = "minlab"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.3")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.6.0")
+
+    implementation("com.aallam.openai:openai-client:3.7.1")
+    implementation("io.ktor:ktor-client-apache5:2.3.0")
+    implementation ("org.reactivestreams:reactive-streams:1.0.3")
+    implementation ("org.springframework.boot:spring-boot-starter-webflux")
+
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
