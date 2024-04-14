f3415091d43154e42bbd833e6660ae8e7471aa31
stopmin <geemin0@naver.com>
2024-04-14 14:42:25 +0900

    feat: ImageOrder register Controller 생성

--- src/main/kotlin/minlab/imageorder/order/presentation/RegisterOrderController.kt
+++ src/main/kotlin/minlab/imageorder/order/presentation/RegisterOrderController.kt
@@ -1,11 +1,8 @@
 package minlab.imageorder.order.presentation
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import minlab.imageorder.common.ResultDTO
-import minlab.imageorder.order.application.GetTextOrderService
-import minlab.imageorder.order.application.RegisterOrderService
-import minlab.imageorder.order.application.TextImageRequestDTO
-import minlab.imageorder.order.application.TextImageResponseDTO
+import minlab.imageorder.order.application.*
+import minlab.imageorder.order.domain.OrderRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -13,24 +10,34 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class RegisterOrderController(
-    private val getTextOrderService: GetTextOrderService,
-    private val registerOrderService: RegisterOrderService,
-    private val objectMapper: ObjectMapper
+    private val aiTextOrderService: AITextOrderService,
+    private val naverOcrService: NaverOcrService,
+    private val orderRepository: OrderRepository
 ) {
-    @PostMapping("/text-order")
-    fun getTextOrder(@RequestBody textImageRequestDTO: TextImageRequestDTO): ResponseEntity<ResultDTO> {
-        val textOrder = getTextOrderService.getTextOrder(textImageRequestDTO)
-        val textImageResponseDTO = objectMapper.readValue(textOrder.toString(), TextImageResponseDTO::class.java)
+    @PostMapping("/register/image-order")
+    suspend fun getTextOrder(@RequestBody registerDTO: RegisterDTO): ResponseEntity<ResultDTO> {
+        val ocrImageRequestDTO = OCRImageRequestDTO(
+            timestamp = registerDTO.timestamp,
+            images = listOf(
+                Image(url = registerDTO.orderImageUrl)
+            )
+        )
 
+        val ocrTextResponse: OCRTextResponseDTO? = naverOcrService.convertImageToText(ocrImageRequestDTO)
+        val generateOrder = aiTextOrderService.generateOrder(ocrTextResponse)
 
+        orderRepository.save(generateOrder.mapToDomain())
         return ResponseEntity.ok(
             ResultDTO(
-                message = "text order success",
-                result = textImageResponseDTO
+                message = "오더 변환 완료",
+                result = generateOrder
             )
         )
     }
 
 }
 
-
+data class RegisterDTO(
+    val orderImageUrl: String,
+    val timestamp: String
+)
