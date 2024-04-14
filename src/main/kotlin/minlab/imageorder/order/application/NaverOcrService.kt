393d2487ba1293d9a9821f8c5e95b7818c0128fa
stopmin <geemin0@naver.com>
2024-04-14 14:41:44 +0900

    feat: OCRTextResponseDTO 기본값 설정

--- src/main/kotlin/minlab/imageorder/order/application/NaverOcrService.kt
+++ src/main/kotlin/minlab/imageorder/order/application/NaverOcrService.kt
@@ -1,14 +1,13 @@
 package minlab.imageorder.order.application
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpStatusCode
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestClient
 import org.springframework.web.client.body
 
 @Service
-class GetTextOrderService(
+class NaverOcrService(
     private val restClient: RestClient
 ) {
     @Value("\${naver.clova.X-OCR-URL}")
@@ -17,12 +16,14 @@ class GetTextOrderService(
     @Value("\${naver.clova.X-OCR-SECRET}")
     private lateinit var clovaKey: String
 
-    fun getTextOrder(textImageRequestDTO: TextImageRequestDTO): Any? {
-        val result = postRequest(clovaUrl, clovaKey, textImageRequestDTO).body<Any>()
+
+    fun convertImageToText(ocrImageRequestDTO: OCRImageRequestDTO): OCRTextResponseDTO? {
+        val result = postRequest(clovaUrl, clovaKey, ocrImageRequestDTO).body<OCRTextResponseDTO>()
         return result
     }
 
-    fun postRequest(uri: String, apiKey: String, requestDTO: Any): RestClient.ResponseSpec {
+
+    private fun postRequest(uri: String, apiKey: String, requestDTO: Any): RestClient.ResponseSpec {
         return restClient.post()
             .uri(uri)
             .header("Content-Type", "application/json")
@@ -35,42 +36,61 @@ class GetTextOrderService(
 
 
 data class Image(
-    val format: String,
-    val name: String,
-    val data: String?,
+    val format: String = "png",
+    val name: String = "medium",
+    val data: String? = null,
     val url: String
 )
 
-data class TextImageRequestDTO(
+data class OCRImageRequestDTO(
     val images: List<Image>,
-    val lang: String,
-    val requestId: String,
-    val resultType: String,
+    val lang: String = "ko",
+    val requestId: String = "string",
+    val resultType: String = "string",
     val timestamp: String,
-    val version: String
+    val version: String = "V2"
 )
 
-data class TextImageResponseDTO(
+data class OCRTextResponseDTO(
     val version: String,
     val requestId: String,
     val timestamp: Long,
     val images: List<ImageResult>
-)
+) {
+    fun mapToText(): String {
+//        return images.flatMap { it.fields?.map { field -> field.inferText } }
+        return images.joinToString("\n") {
+            it.inferResult ?: ""
+        }
+    }
+}
 
 data class ImageResult(
     val uid: String?,
     val name: String?,
     val inferResult: String?,
     val message: String?,
+    val validationResult: ValidationResult?,
     val fields: List<Field>?,
-    val validationResult: ValidationResult?
 )
 
 data class Field(
+    val valueType: String?,
+    val boundingPoly: BoundingPoly?,
     val inferText: String?,
     val inferConfidence: Double?
+)
+
+data class BoundingPoly(
+    val vertices: List<Vertex>?
+)
+
+data class Vertex(
+    val x: Int,
+    val y: Int
 )
 
 data class ValidationResult(
     val result: String?
 )
+
